d1b116a908cef624275f243f9859de4d628a88f0
nicolemaracaja <nicole.brito.maracaja@ccc.ufcg.edu.br>
2025-05-13 09:55:59 -0300

    ajustes no formulário

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista(){
-    const tarefas = [
-        {tarefa: 'React', tempo: '02:00:00'},
-        {tarefa: 'Javascript', tempo: '01:00:00'},
-        {tarefa: 'Typescript', tempo: '03:00:00'}
-    ]
+
+function Lista( { tarefas }: { tarefas: ITarefa[]}){
+    
+
     return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
