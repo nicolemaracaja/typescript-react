e4136d44ab4e93e243d8769cc77acf953017d086
nicolemaracaja <nicole.brito.maracaja@ccc.ufcg.edu.br>
2025-05-14 11:26:42 -0300

    cronometro funcionando

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import style from './Relogio.module.scss';
 
-export default function Relogio(){
+interface Props {
+    tempo: number | undefined
+}
+
+export default function Relogio({ tempo = 0}: Props){
+    const minutos = Math.floor(tempo / 60);
+    const segundos = tempo % 60;
+
+    const [minutosDezena, minutosUnidade] = String(minutos).padStart(2, '0');
+    const [segundosDezena, segundosUnidade] = String(segundos).padStart(2, '0');
     return(
         //mesma coisa que um React.Fragment
         <> 
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioNumero}>{minutosDezena}</span>
+            <span className={style.relogioNumero}>{minutosUnidade}</span>
             <span className={style.relogioDivisao}>:</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioNumero}>{segundosDezena}</span>
+            <span className={style.relogioNumero}>{segundosUnidade}</span>
         </>
     )
 }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,17 +2,23 @@ import Item from './Item';
 import style from './Lista.module.scss';
 import { ITarefa } from '../../types/tarefa';
 
+interface Props{
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 
-function Lista( { tarefas }: { tarefas: ITarefa[]}){
+
+function Lista( { tarefas, selecionaTarefa }: Props){
     
 
     return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) => (
+                {tarefas.map((item) => (
                     <Item 
-                        key={index}
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
                         {...item} //pega todos os itens do objeto
                     />
                 ))}
